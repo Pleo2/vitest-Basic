fb22e8fbdd6e629bd79d305665cd9bd4ca427cc1
Pleo2/Jose Moreno <leooel23m@gmail.com>
2023-12-16 00:55:56 -0400

    change: changes in the fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,9 +2,6 @@ export const fizzbuzz = (num) => {
     if (typeof num !== 'number') throw new Error('parameter provided must be a number')
     if (Number.isNaN(num)) throw new Error('parameter provided must be a number, the parameter is NaN')
 
-    // if(num % 15 === 0) return 'fizzbuzz'
-    // if(num % 5 === 0) return 'buzz'
-    // if(num % 3 === 0) return 'fizz'
     let output = ''
     const multiples = {
         3 : 'fizz',
